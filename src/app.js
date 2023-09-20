d5e47110a1286cc0e797031763842439c3bd8724
Marina Ivanova <Marjanja7@mail.ru>
2023-09-20 16:48:18 +0300

    11

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 let currentReviewIndex =0
-let start = 0
-let end = start + 3
+/*let start = 0
+let end = start + 3*/
 let reviews = [
     {
         title: 'Йошкар-Ола из Казани на машине:)',
@@ -47,11 +47,9 @@ function carousel () {
     let container = document.getElementById('container');
     let review = reviews[currentReviewIndex];
     container.innerHTML = `
-        <div class='carousel'>
         <h2 class="title">${tour.title}</h2>
         <div class="photo">
             <img class="image" src="${tour.photo}"/>
-             
             <span class="quote-icon"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path></svg></span>
             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path></svg>
         </div>
@@ -73,7 +71,7 @@ next.addEventListener('click',nextReview)
 const random = document.getElementById('random-btn');
 random.addEventListener('click',randomReview)
 
-function prevReview() {
+/*function prevReview() {
     if (currentReviewIndex<=0) {
         currentReviewIndex = reviews.end;
     }else {
@@ -89,7 +87,7 @@ function nextReview() {
         currentReviewIndex = currentReviewIndex + 1;
     }
     carousel ();
-}
+}*/
 function randomBtn() {
     let currentRandomIndex = Math.floor(Math.random() * reviews.length);
     currentReviewIndex = currentRandomIndex;
